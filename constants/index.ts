6bf987f983a98bc91176fe1f550aab167594f673
Metwally-Sayed <metwallysayed1999@gmail.com>
2022-09-25 16:46:06 +0200

    navbar

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,5 @@
 export const Base_Url = "http://localhost:3000";
+
+export function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <Header />;
+};
 
-export default Home
+export default Home;
