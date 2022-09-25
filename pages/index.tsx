7b989debb02b339d86cc5292c50d8ed204d6e7bb
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2022-09-25 19:04:54 +0200

    Add story component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Story from '../components/story'
+import { stories } from '../constants'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Story  stories={stories} />
   )
 }
 
