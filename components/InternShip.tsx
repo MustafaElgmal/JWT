60b7a9625679b2d7e9fbffa14265d897d7c38e4a
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2022-09-27 10:00:45 +0200

    Add scroll smothe on internships and oppertinties sections

--- components/InternShip.tsx
+++ components/InternShip.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function InternShip() {
     return (
-        <div className=" 2xl:container 2xl:mx-auto md:py-5 lg:px-5 md:px-6 py-9 px-4">
+        <div id="internships" className="2xl:container 2xl:mx-auto md:py-5 lg:px-5 md:px-6 py-9 px-4">
             <h2 className="text-2xl lg:text-6xl font-black">InternShips</h2>
             <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:grap-8 md:gap-6 gap-4 mt-10">
                 <div className="relative group">
