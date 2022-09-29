5c819bae9ac32be482ace4ed692c148e8dc2b1f9
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2022-09-29 11:53:47 +0200

    Add type of story

--- components/Opportunities.tsx
+++ components/Opportunities.tsx
@@ -67,30 +67,3 @@ function Opportunities({ careers }: AppProps) {
     );
 }
 export default Opportunities;
-
-{/* <div className="pb-16">
-    <div className="container mx-auto">
-        <div className="flex flex-wrap items-center pb-12">
-            <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
-                <h1 className="text-xl lg:text-4xl  font-black">Animator</h1>
-                <div className="py-2 text-color">
-                    <h2 className="text-lg lg:text-3xl  py-4 md:py-8">
-                        We are looking for an Animator  with 3+ years of experience.
-                        If you got the moves, show us!
-                    </h2>
-                    <div className="flex items-center cursor-pointer pb-4 md:pb-0">
-                        <h3 className=" text-lg lg:text-2xl font-semibold underline text-indigo-700">Come join us!</h3>
-                        <div className="pl-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
-                                <path d="M13.1719 12L8.22192 7.04999L9.63592 5.63599L15.9999 12L9.63592 18.364L8.22192 16.95L13.1719 12Z" fill="#D53F8C" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                <img className="absolute w-full h-full inset-0  object-center" src="https://lxwnzovjjfyhbnrqtzqh.supabase.co/storage/v1/object/public/jwt/hiring/309196379_409566584660863_3029818054303487200_n.jpg" alt="" />
-            </div>
-        </div>
-    </div>
-</div> */}
