4b9be32f3ff41bad88e10d996b891318f19c6ba8
FahadAlshemali <fhadfhad222@gmail.com>
2022-09-29 12:31:06 +0200

    update

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -1,6 +1,11 @@
 import { useFormik } from "formik";
 import React from "react";
-import { Bars3Icon, EnvelopeIcon, PhoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  Bars3Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 
 type Props = {};
 
@@ -12,13 +17,19 @@ const ContactForm = (props: Props) => {
       number: "",
       message: "",
     },
-    onSubmit: async (values) => { },
+    onSubmit: async (values) => {},
   });
   return (
-    <div id='contact us' className="w-full flex items-center justify-center mb-10">
+    <div
+      id="contact us"
+      className="w-full flex items-center justify-center mb-10"
+    >
       <section className="relative bg-white" aria-labelledby="contact-heading">
-      <h1 className="text-2xl lg:text-6xl font-black mb-10">Contact Us</h1>
-        <div className="absolute h-1/2 w-full bg-warm-gray-50" aria-hidden="true" />
+        <h1 className="text-2xl lg:text-6xl font-black mb-10">Contact Us</h1>
+        <div
+          className="absolute h-1/2 w-full bg-warm-gray-50"
+          aria-hidden="true"
+        />
         {/* Decorative dot pattern */}
 
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -26,7 +37,10 @@ const ContactForm = (props: Props) => {
             <div className="grid grid-cols-1 lg:grid-cols-3">
               {/* Contact information */}
               <div className="relative overflow-hidden bg-gradient-to-b from-blue-700 to-blue-600 py-10 px-6 sm:px-10 xl:p-12">
-                <div className="pointer-events-none absolute inset-0 sm:hidden" aria-hidden="true">
+                <div
+                  className="pointer-events-none absolute inset-0 sm:hidden"
+                  aria-hidden="true"
+                >
                   <svg
                     className="absolute inset-0 h-full w-full"
                     width={343}
@@ -122,24 +136,32 @@ const ContactForm = (props: Props) => {
                     </defs>
                   </svg>
                 </div>
-                <h3 className="text-lg font-medium text-white">Contact information</h3>
+                <h3 className="text-lg font-medium text-white">
+                  Contact information
+                </h3>
                 <p className="mt-6 max-w-3xl text-base text-teal-50">
-                  Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor
-                  lacus arcu.
+                  Nullam risus blandit ac aliquam justo ipsum. Quam mauris
+                  volutpat massa dictumst amet. Sapien tortor lacus arcu.
                 </p>
                 <dl className="mt-8 space-y-6">
                   <dt>
                     <span className="sr-only">Phone number</span>
                   </dt>
                   <dd className="flex text-base text-teal-50">
-                    <PhoneIcon className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
+                    <PhoneIcon
+                      className="h-6 w-6 flex-shrink-0 text-teal-200"
+                      aria-hidden="true"
+                    />
                     <span className="ml-3">+1 (555) 123-4567</span>
                   </dd>
                   <dt>
                     <span className="sr-only">Email</span>
                   </dt>
                   <dd className="flex text-base text-teal-50">
-                    <EnvelopeIcon className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
+                    <EnvelopeIcon
+                      className="h-6 w-6 flex-shrink-0 text-teal-200"
+                      aria-hidden="true"
+                    />
                     <span className="ml-3">support@workcation.com</span>
                   </dd>
                 </dl>
@@ -147,7 +169,12 @@ const ContactForm = (props: Props) => {
                   <li>
                     <a className="text-teal-200 hover:text-teal-100" href="#">
                       <span className="sr-only">Facebook</span>
-                      <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                      <svg
+                        className="h-7 w-7"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
                         <path
                           fillRule="evenodd"
                           d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
@@ -159,7 +186,12 @@ const ContactForm = (props: Props) => {
                   <li>
                     <a className="text-teal-200 hover:text-teal-100" href="#">
                       <span className="sr-only">GitHub</span>
-                      <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                      <svg
+                        className="h-7 w-7"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
                         <path
                           fillRule="evenodd"
                           d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -171,7 +203,12 @@ const ContactForm = (props: Props) => {
                   <li>
                     <a className="text-teal-200 hover:text-teal-100" href="#">
                       <span className="sr-only">Twitter</span>
-                      <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                      <svg
+                        className="h-7 w-7"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
                         <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                       </svg>
                     </a>
@@ -181,10 +218,19 @@ const ContactForm = (props: Props) => {
 
               {/* Contact form */}
               <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-                <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
-                <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                <h3 className="text-lg font-medium text-warm-gray-900">
+                  Send us a message
+                </h3>
+                <form
+                  action="#"
+                  method="POST"
+                  className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+                >
                   <div>
-                    <label htmlFor="first-name" className="block text-sm font-medium text-warm-gray-900">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-warm-gray-900"
+                    >
                       First name
                     </label>
                     <div className="mt-1">
@@ -198,7 +244,10 @@ const ContactForm = (props: Props) => {
                     </div>
                   </div>
                   <div>
-                    <label htmlFor="last-name" className="block text-sm font-medium text-warm-gray-900">
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium text-warm-gray-900"
+                    >
                       Last name
                     </label>
                     <div className="mt-1">
@@ -212,7 +261,10 @@ const ContactForm = (props: Props) => {
                     </div>
                   </div>
                   <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-warm-gray-900">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-warm-gray-900"
+                    >
                       Email
                     </label>
                     <div className="mt-1">
@@ -227,10 +279,16 @@ const ContactForm = (props: Props) => {
                   </div>
                   <div>
                     <div className="flex justify-between">
-                      <label htmlFor="phone" className="block text-sm font-medium text-warm-gray-900">
+                      <label
+                        htmlFor="phone"
+                        className="block text-sm font-medium text-warm-gray-900"
+                      >
                         Phone
                       </label>
-                      <span id="phone-optional" className="text-sm text-warm-gray-500">
+                      <span
+                        id="phone-optional"
+                        className="text-sm text-warm-gray-500"
+                      >
                         Optional
                       </span>
                     </div>
@@ -247,10 +305,16 @@ const ContactForm = (props: Props) => {
                   </div>
                   <div className="sm:col-span-2">
                     <div className="flex justify-between">
-                      <label htmlFor="message" className="block text-sm font-medium text-warm-gray-900">
+                      <label
+                        htmlFor="message"
+                        className="block text-sm font-medium text-warm-gray-900"
+                      >
                         Message
                       </label>
-                      <span id="message-max" className="text-sm text-warm-gray-500">
+                      <span
+                        id="message-max"
+                        className="text-sm text-warm-gray-500"
+                      >
                         Max. 500 characters
                       </span>
                     </div>
@@ -261,7 +325,7 @@ const ContactForm = (props: Props) => {
                         rows={4}
                         className="block w-full rounded-md border-warm-gray-300 py-3 px-4 text-warm-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                         aria-describedby="message-max"
-                        defaultValue={''}
+                        defaultValue={""}
                       />
                     </div>
                   </div>
